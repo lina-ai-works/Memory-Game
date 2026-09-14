238cf7cf90d37dbe1dae3cc0e602c965fd17b9b1
lina-ai-works <m.iris5590@gmail.com>
2026-09-15 01:11:52 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Memory-Game/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
